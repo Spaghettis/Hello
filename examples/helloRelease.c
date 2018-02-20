1d29d6b34ed42895deb870ffb231dbd56e7621e4
nicolasdanet <nicolas.danet@free.fr>
2018-02-20 14:48:00 +0100

    Fix typo

--- examples/helloRelease.c
+++ examples/helloRelease.c
@@ -62,7 +62,7 @@ PD_STUB void helloRelease_destroy (void)
 {
     /* Here you can release an external resource acquired. */
     
-    class_free (hello_class);   /* Requiered to track memory leaks. */
+    class_free (hello_class);   /* Required to track memory leaks. */
 }
 
 // -----------------------------------------------------------------------------------------------------------
